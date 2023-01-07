1d999c02eec914592232f41622d9ad01bc896ad1
NirmalPatel17 <nirmaldpatel2003@gmail.com>
2023-01-07 09:07:32 +0530

    create otp.html

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'realestate'
+
 urlpatterns = [
     path('',views.index, name="index"),
     path('auth/register/',views.register, name="register"),
     path('auth/login/',views.login, name="login"),
+    path('auth/register/otp/<phoneno>/',views.otp, name="otp"),
+
+    path('marketplace/',views.marketplace, name="marketplace"),
+    path('property/',views.property, name="property"),
+
     path('contact/',views.contact, name="contact"),
     path('about/',views.about, name="about"),
     path('terms&conditions/',views.tc, name="tc"),
     path('privacypolicy/',views.privacypolicy, name="privacypolicy"),
-    path('marketplace/',views.marketplace, name="marketplace"),
-    path('property/',views.property, name="property"),
 ]
--- marketplace/views.py
+++ marketplace/views.py
@@ -1,12 +1,45 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def register(request):
+    if request.method == 'POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        emailid = request.POST.get('emailid')
+        password = request.POST.get('password')
+        phoneno = request.POST.get('phoneno')
+        usertype = request.POST.get('usertype')
+        list1 = [fname,lname,emailid,password,phoneno,usertype]
+        print(list1)
+
+        try:
+            if len(password) < 6:
+                messages.error(request,'Password is too short')
+            
+            elif len(phoneno) < 10:
+                messages.error(request,'Contact no is wrong')
+
+            else:
+                # response = 'otp/'+phoneno+'/'
+
+                return redirect(reverse('realestate:otp',kwargs={'phoneno':phoneno}))
+
+        except Exception as e:
+            print(e)
+            return redirect('auth/register/')
+
     return render(request,'register.html')
-    
+        
+
+def otp(request, phoneno):
+    context = {'phoneno': phoneno}
+    return render(request,'otp.html', context)
+
 def login(request):
     return render(request,'login.html')
 
